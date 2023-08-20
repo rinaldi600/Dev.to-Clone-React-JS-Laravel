576416edf13e05c45295745582056102369118ec
rinaldi600 <rinaldih84@gmail.com>
2023-08-20 13:56:07 +0700

    Implementation notifications 8

--- dev-to-app/public/js/app.js
+++ dev-to-app/public/js/app.js
@@ -10877,16 +10877,50 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+/* harmony import */ var _Pages_Layout_Layout__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/Pages/Layout/Layout */ "./resources/js/Pages/Layout/Layout.js");
+/* harmony import */ var _inertiajs_inertia_react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @inertiajs/inertia-react */ "./node_modules/@inertiajs/inertia-react/dist/index.js");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+
 
 
 function DetailComment() {
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
-    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {
-      children: "WORK"
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.Fragment, {
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+      className: "min-h-screen pt-5 font-['Segoe_UI'] bg-[#F5F5F5]",
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("div", {
+        className: "max-w-[1024px] mx-auto min-h-[777.2px]",
+        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+          className: "mx-auto bg-white min-h-[139.2px] grid items-center rounded-t-lg shadow-[0px_1px_3px_0px_rgba(0,0,0,0.02),0px_0px_0px_1px_rgba(27,31,35,0.15)] max-w-[960px] pl-5",
+          children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("div", {
+              children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsxs)("h1", {
+                className: "text-[#717171] text-2xl",
+                children: ["Discussion on: ", /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("span", {
+                  className: "font-bold text-[#242424]",
+                  children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_inertiajs_inertia_react__WEBPACK_IMPORTED_MODULE_1__.Link, {
+                    children: "TEST TRIAL"
+                  })
+                })]
+              }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_inertiajs_inertia_react__WEBPACK_IMPORTED_MODULE_1__.Link, {
+                children: "View post"
+              })]
+            })
+          })
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+          className: "mx-auto bg-white min-h-[541px] rounded-lg shadow-[0px_1px_3px_0px_rgba(0,0,0,0.02),0px_0px_0px_1px_rgba(27,31,35,0.15)] max-w-[992px]"
+        })]
+      })
     })
   });
 }
+DetailComment.layout = function (page) {
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(_Pages_Layout_Layout__WEBPACK_IMPORTED_MODULE_0__["default"], {
+    children: page,
+    title: "Welcome"
+  });
+};
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (DetailComment);
 
 /***/ }),
--- dev-to-app/resources/js/Pages/Profile/Notifications/DetailComment/DetailComment.js
+++ dev-to-app/resources/js/Pages/Profile/Notifications/DetailComment/DetailComment.js
@@ -1,9 +1,28 @@
+import Layout from "@/Pages/Layout/Layout";
+import { Link } from "@inertiajs/inertia-react";
+
 function DetailComment() {
     return (
         <>
-            <div>WORK</div>
+            <div className="min-h-screen pt-5 font-['Segoe_UI'] bg-[#F5F5F5]">
+                <div className="max-w-[1024px] mx-auto min-h-[777.2px]">
+                    <div className="mx-auto bg-white min-h-[139.2px] grid items-center rounded-t-lg shadow-[0px_1px_3px_0px_rgba(0,0,0,0.02),0px_0px_0px_1px_rgba(27,31,35,0.15)] max-w-[960px] pl-5">
+                        <div>
+                            <div>
+                                <h1 className="text-[#717171] text-2xl">Discussion on: <span className="font-bold text-[#242424]"><Link>TEST TRIAL</Link></span></h1>
+                                <Link>View post</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mx-auto bg-white min-h-[541px] rounded-lg shadow-[0px_1px_3px_0px_rgba(0,0,0,0.02),0px_0px_0px_1px_rgba(27,31,35,0.15)] max-w-[992px]">
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
+
+DetailComment.layout = page => <Layout children={page} title="Welcome" />
 
 export default DetailComment;
